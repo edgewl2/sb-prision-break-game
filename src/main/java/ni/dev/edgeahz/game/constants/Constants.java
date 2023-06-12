04eb5bfaeb89605a4b7d720d3c0732f293ebc5a5
Gerardo Hernandez <edgewl2@gmail.com>
2023-06-12 03:49:56 -0600

    feat(constants): constant variables used
    Utility class with constant variables used in the project.

---
+++ src/main/java/ni/dev/edgeahz/game/constants/Constants.java
@@ -0,0 +1,17 @@
+package ni.dev.edgeahz.game.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+
+    public static final char PRISONER = 'P';
+    public static final char EXIT = 'S';
+    public static final char WALL = '|';
+    public static final char GUARD_UP = '^';
+    public static final char GUARD_DOWN = 'v';
+    public static final char GUARD_RIGHT = '>';
+    public static final char GUARD_LEFT = '<';
+    public static final String OBSTRUCTIONS = "|><^v";
+    public static final String DELIMITER = ",";
+}
