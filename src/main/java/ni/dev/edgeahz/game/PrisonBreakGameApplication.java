ffbd6d705b888d3198301a846d8abbdf4d25ee00
Gerardo Hernandez <edgewl2@gmail.com>
2023-06-12 03:25:12 -0600

    refactor(project): rename again
    Classes are renamed as upgrade.

--- src/main/java/ni/dev/edgeahz/game/PrisonBreakGameApplication.java
+++ src/main/java/ni/dev/edgeahz/game/PrisonBreakGameApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrisionBreakGameApplication {
+public class PrisonBreakGameApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PrisionBreakGameApplication.class, args);
+        SpringApplication.run(PrisonBreakGameApplication.class, args);
     }
 
 }
